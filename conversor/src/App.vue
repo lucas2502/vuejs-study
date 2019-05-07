8c2f2182d4204fcfa387d093fcf9c55e67613a15
lucas2502 <lucas2502@live.com>
2019-05-06 23:44:20 -0300

    Finish create coin converter in vuejs

--- conversor/src/App.vue
+++ conversor/src/App.vue
@@ -1,6 +1,18 @@
 <template>
   <div id="app">
+    <h1>CONVERSOR DE MOEDAS</h1>
+    <div class="row">
      <Conversor moeda-a="USD" moeda-b="BRL"/>
+     <Conversor moeda-a="BRL" moeda-b="USD"/>
+    </div>
+    <div class="row">
+     <Conversor moeda-a="CAD" moeda-b="BRL"/>
+     <Conversor moeda-a="BRL" moeda-b="CAD"/>
+    </div>
+    <div class="row">
+     <Conversor moeda-a="EUR" moeda-b="BRL"/>
+     <Conversor moeda-a="BRL" moeda-b="EUR"/>
+    </div>
   </div>
 </template>
 
@@ -16,11 +28,19 @@ export default {
 
 <style>
 #app {
+  margin: auto;
+  max-width: 900px;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.row{
+  margin-top: 40px;
+  display: flex;
+  justify-content: space-around;
+
 }
 </style>
--- conversor/src/components/Conversor.vue
+++ conversor/src/components/Conversor.vue
@@ -38,5 +38,11 @@ export default {
 </script>
 
 <style scoped>
+.conversor {
+    padding: 20px;
+    max-width: 300px;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2)
+}
+
 
 </style>
